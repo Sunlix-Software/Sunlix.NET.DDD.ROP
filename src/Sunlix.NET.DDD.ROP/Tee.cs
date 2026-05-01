c7466b0418ca35606c4b5adae34d07c75f56e09a
igor-bzovsky <igor.bzovsky@gmail.com>
2026-05-01 21:37:45 +0300

    Added XML docs for Tee and TeeError

--- src/Sunlix.NET.DDD.ROP/Tee.cs
+++ src/Sunlix.NET.DDD.ROP/Tee.cs
@@ -2,12 +2,14 @@
 {
     public abstract partial class Result<TSuccess, TFailure>
     {
+        /// <include file="XmlDocs/Tee.xml" path="doc/members/member[@name='M:Sunlix.NET.DDD.ROP.Result`2.Tee(System.Action{`0})']/*" />
         public Result<TSuccess, TFailure> Tee(Action<TSuccess> sideEffect)
         {
             _ = sideEffect ?? throw new ArgumentNullException(nameof(sideEffect));
             return TeeInternal(sideEffect);
         }
 
+        /// <include file="XmlDocs/Tee.xml" path="doc/members/member[@name='M:Sunlix.NET.DDD.ROP.Result`2.TeeAsync(System.Func{`0,System.Threading.Tasks.Task})']/*" />
         public async Task<Result<TSuccess, TFailure>> TeeAsync(Func<TSuccess, Task> sideEffectAsync)
         {
             _ = sideEffectAsync ?? throw new ArgumentNullException(nameof(sideEffectAsync));
@@ -15,7 +17,9 @@
         }
 
 
+        /// <include file="XmlDocs/Tee.xml" path="doc/members/member[@name='M:Sunlix.NET.DDD.ROP.Result`2.TeeInternal(System.Action{`0})']/*" />
         protected abstract Result<TSuccess, TFailure> TeeInternal(Action<TSuccess> sideEffect);
+        /// <include file="XmlDocs/Tee.xml" path="doc/members/member[@name='M:Sunlix.NET.DDD.ROP.Result`2.TeeInternalAsync(System.Func{`0,System.Threading.Tasks.Task})']/*" />
         protected abstract Task<Result<TSuccess, TFailure>> TeeInternalAsync(Func<TSuccess, Task> sideEffectAsync);
 
 
@@ -43,8 +47,10 @@
         }
     }
 
+    /// <include file="XmlDocs/Tee.xml" path="doc/members/member[@name='T:Sunlix.NET.DDD.ROP.TeeExtensions']/*" />
     public static class TeeExtensions
     {
+        /// <include file="XmlDocs/Tee.xml" path="doc/members/member[@name='M:Sunlix.NET.DDD.ROP.TeeExtensions.Tee``2(System.Threading.Tasks.Task{Sunlix.NET.DDD.ROP.Result{``0,``1}},System.Action{``0})']/*" />
         public static async Task<Result<TSuccess, TFailure>> Tee<TSuccess, TFailure>(
             this Task<Result<TSuccess, TFailure>> taskResult,
             Action<TSuccess> sideEffect)
@@ -55,6 +61,7 @@
             return result.Tee(sideEffect);
         }
 
+        /// <include file="XmlDocs/Tee.xml" path="doc/members/member[@name='M:Sunlix.NET.DDD.ROP.TeeExtensions.TeeAsync``2(System.Threading.Tasks.Task{Sunlix.NET.DDD.ROP.Result{``0,``1}},System.Func{``0,System.Threading.Tasks.Task})']/*" />
         public static async Task<Result<TSuccess, TFailure>> TeeAsync<TSuccess, TFailure>(
             this Task<Result<TSuccess, TFailure>> taskResult,
             Func<TSuccess, Task> sideEffectAsync)
--- src/Sunlix.NET.DDD.ROP/TeeError.cs
+++ src/Sunlix.NET.DDD.ROP/TeeError.cs
@@ -2,12 +2,14 @@
 {
     public abstract partial class Result<TSuccess, TFailure>
     {
+        /// <include file="XmlDocs/TeeError.xml" path="doc/members/member[@name='M:Sunlix.NET.DDD.ROP.Result`2.TeeError(System.Action{`1})']/*" />
         public Result<TSuccess, TFailure> TeeError(Action<TFailure> sideEffect)
         {
             _ = sideEffect ?? throw new ArgumentNullException(nameof(sideEffect));
             return TeeErrorInternal(sideEffect);
         }
 
+        /// <include file="XmlDocs/TeeError.xml" path="doc/members/member[@name='M:Sunlix.NET.DDD.ROP.Result`2.TeeErrorAsync(System.Func{`1,System.Threading.Tasks.Task})']/*" />
         public async Task<Result<TSuccess, TFailure>> TeeErrorAsync(Func<TFailure, Task> sideEffectAsync)
         {
             _ = sideEffectAsync ?? throw new ArgumentNullException(nameof(sideEffectAsync));
@@ -15,7 +17,9 @@
         }
 
 
+        /// <include file="XmlDocs/TeeError.xml" path="doc/members/member[@name='M:Sunlix.NET.DDD.ROP.Result`2.TeeErrorInternal(System.Action{`1})']/*" />
         protected abstract Result<TSuccess, TFailure> TeeErrorInternal(Action<TFailure> sideEffect);
+        /// <include file="XmlDocs/TeeError.xml" path="doc/members/member[@name='M:Sunlix.NET.DDD.ROP.Result`2.TeeErrorInternalAsync(System.Func{`1,System.Threading.Tasks.Task})']/*" />
         protected abstract Task<Result<TSuccess, TFailure>> TeeErrorInternalAsync(Func<TFailure, Task> sideEffectAsync);
 
 
@@ -45,8 +49,10 @@
         }
     }
 
+    /// <include file="XmlDocs/TeeError.xml" path="doc/members/member[@name='T:Sunlix.NET.DDD.ROP.TeeErrorExtensions']/*" />
     public static class TeeErrorExtensions
     {
+        /// <include file="XmlDocs/TeeError.xml" path="doc/members/member[@name='M:Sunlix.NET.DDD.ROP.TeeErrorExtensions.Tee``2(System.Threading.Tasks.Task{Sunlix.NET.DDD.ROP.Result{``0,``1}},System.Action{``1})']/*" />
         public static async Task<Result<TSuccess, TFailure>> Tee<TSuccess, TFailure>(
             this Task<Result<TSuccess, TFailure>> taskResult,
             Action<TFailure> sideEffect)
@@ -57,6 +63,7 @@
             return result.TeeError(sideEffect);
         }
 
+        /// <include file="XmlDocs/TeeError.xml" path="doc/members/member[@name='M:Sunlix.NET.DDD.ROP.TeeErrorExtensions.TeeAsync``2(System.Threading.Tasks.Task{Sunlix.NET.DDD.ROP.Result{``0,``1}},System.Func{``1,System.Threading.Tasks.Task})']/*" />
         public static async Task<Result<TSuccess, TFailure>> TeeAsync<TSuccess, TFailure>(
             this Task<Result<TSuccess, TFailure>> taskResult,
             Func<TFailure, Task> sideEffectAsync)

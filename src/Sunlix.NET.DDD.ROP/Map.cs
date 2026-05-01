09be95f2af9147d25d230bf3ad5e431f696eb171
igor-bzovsky <igor.bzovsky@gmail.com>
2026-05-01 20:01:39 +0300

    Added XML docs for Map and XML docs fixes for Bind

--- src/Sunlix.NET.DDD.ROP/Map.cs
+++ src/Sunlix.NET.DDD.ROP/Map.cs
@@ -4,19 +4,23 @@ namespace Sunlix.NET.DDD.ROP
 {
     public abstract partial class Result<TSuccess, TFailure>
     {
+        /// <include file="XmlDocs/Map.xml" path="doc/members/member[@name='M:Sunlix.NET.DDD.ROP.Result`2.Map``1(System.Func{`0,``0})']/*" />
         public Result<TNewSuccess, TFailure> Map<TNewSuccess>(Func<TSuccess, TNewSuccess> mapFunction)
         {
             _ = mapFunction ?? throw new ArgumentNullException(nameof(mapFunction));
             return MapInternal(mapFunction);
         }
 
+        /// <include file="XmlDocs/Map.xml" path="doc/members/member[@name='M:Sunlix.NET.DDD.ROP.Result`2.MapAsync``1(System.Func{`0,System.Threading.Tasks.Task{``0}})']/*" />
         public async Task<Result<TNewSuccess, TFailure>> MapAsync<TNewSuccess>(Func<TSuccess, Task<TNewSuccess>> mapFunctionAsync)
         {
             _ = mapFunctionAsync ?? throw new ArgumentNullException(nameof(mapFunctionAsync));
             return await MapInternalAsync(mapFunctionAsync).ConfigureAwait(false);
         }
 
+        /// <include file="XmlDocs/Map.xml" path="doc/members/member[@name='M:Sunlix.NET.DDD.ROP.Result`2.MapInternal``1(System.Func{`0,``0})']/*" />
         protected abstract Result<TNewSuccess, TFailure> MapInternal<TNewSuccess>(Func<TSuccess, TNewSuccess> mapFunction);
+        /// <include file="XmlDocs/Map.xml" path="doc/members/member[@name='M:Sunlix.NET.DDD.ROP.Result`2.MapInternalAsync``1(System.Func{`0,System.Threading.Tasks.Task{``0}})']/*" />
         protected abstract Task<Result<TNewSuccess, TFailure>> MapInternalAsync<TNewSuccess>(Func<TSuccess, Task<TNewSuccess>> mapFunctionAsync);
 
         #region Success & Failure
@@ -39,8 +43,10 @@ namespace Sunlix.NET.DDD.ROP
         #endregion
     }
 
+    /// <include file="XmlDocs/Map.xml" path="doc/members/member[@name='T:Sunlix.NET.DDD.ROP.MapExtensions']/*" />
     public static class MapExtensions
     {
+        /// <include file="XmlDocs/Map.xml" path="doc/members/member[@name='M:Sunlix.NET.DDD.ROP.MapExtensions.Map``3(System.Threading.Tasks.Task{Sunlix.NET.DDD.ROP.Result{``1,``2}},System.Func{``1,``0})']/*" />
         public static async Task<Result<TNewSuccess, TFailure>> Map<TNewSuccess, TSuccess, TFailure>(
             this Task<Result<TSuccess, TFailure>> taskResult,
             Func<TSuccess, TNewSuccess> mapFunction)
@@ -52,6 +58,7 @@ namespace Sunlix.NET.DDD.ROP
             return result.Map(mapFunction);
         }
 
+        /// <include file="XmlDocs/Map.xml" path="doc/members/member[@name='M:Sunlix.NET.DDD.ROP.MapExtensions.MapAsync``3(System.Threading.Tasks.Task{Sunlix.NET.DDD.ROP.Result{``1,``2}},System.Func{``1,System.Threading.Tasks.Task{``0}})']/*" />
         public static async Task<Result<TNewSuccess, TFailure>> MapAsync<TNewSuccess, TSuccess, TFailure>(
             this Task<Result<TSuccess, TFailure>> taskResult,
             Func<TSuccess, Task<TNewSuccess>> mapFunctionAsync)
